dcf4f242b1c6537e8df290d7ccc60acff045e469
pinoycolada <pinoydol.colada@gmail.com>
2023-05-19 23:58:41 -0400

    added imports

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
+import { Link } from "react-scroll/modules"
+import { HiArrowDown } from "react-icons/hi"
 const HeroSection = () => {
     return (
         <section id= "home">
