49f06672031df83b8e49b305fefe93ceedbe9566
pinoycolada <pinoydol.colada@gmail.com>
2023-06-01 23:59:07 -0400

    added projects link

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -10,7 +10,17 @@ const HeroSection = () => {
                 <Image/>
                 </div>
                 <div>
-
+                <Link
+            to="projects"
+            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
+            activeClass="active"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            Projects
+          </Link>
                 </div>
             </div>
             <div className="flex flex-row items-center text-center justify-center ">
