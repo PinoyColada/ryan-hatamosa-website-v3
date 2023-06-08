ee4173059f7ed33b2ee77e95dfea381c93c1b87a
pinoycolada <pinoydol.colada@gmail.com>
2023-06-08 15:33:46 -0400

    updated herosection file, added headshot pic

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 import { Link } from "react-scroll/modules"
@@ -7,8 +8,9 @@ const HeroSection = () => {
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
+        <Image className="rounded-full shadow-2x1" src="/headshotpic.JPG" alt="profile photo" width={300} height={300}/>
         </div>
-        <Image/>
+
         <div className="md:mt-2 md:w-3/5">
         <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Ryan!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
