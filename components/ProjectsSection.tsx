61009b3e08fb2c719f803c49d103b656059926b8
pinoycolada <pinoydol.colada@gmail.com>
2023-06-08 18:50:29 -0400

    added project details(images, title name, etc.)

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,5 +1,30 @@
 import React from 'react'
 
+const projects = [
+    {
+        name: "Ryan's Portfolio Website",
+        description:
+            "A website about me and showcases my projects (that you are currently on!) Created with Next.js and Tailwind CSS",
+        image: "/website.jpg",
+        github: "https://github.com/PinoyColada/ryan-hatamosa-website-v3",
+    },
+    {
+        name: "Body-Sculptors",
+        description: "An application where you can record your workouts. Created with MongoDB, Express, React.js, and Node.js (MERN)",
+        image: "/body-sculptors-logo.jpeg",
+        github: "https://github.com/PinoyColada/Body-Sculptors",
+        link: "https://agile-journey-75895.herokuapp.com/",
+    },
+    {
+        name: "Coder Typer",
+        description:
+            "A type racing game by a Programmer, for programmers. Created with HTML, CSS, and Javascript",
+        image: "/coder-typer-logo.png",
+        github: "https://github.com/PinoyColada/Coder-Typer",
+        link: "https://coder-typer.surge.sh/",
+    },
+]
+
 const ProjectsSection = () => {
     return (
         <div>ProjectsSection</div>
