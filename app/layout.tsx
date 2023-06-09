615c7185b7eea1eece3019dd0010b737c23a2121
pinoycolada <pinoydol.colada@gmail.com>
2023-06-08 20:08:26 -0400

    added footer file and Slideup file

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@
 import "../styles/globals.css";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "next-themes";
+import Footer from "@/components/Footer";
+
 export default function RootLayout({
   children,
 }: {
@@ -18,6 +20,7 @@ export default function RootLayout({
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
