ea99e8ca4e2db01fe55939799b004face0f7632c
pinoycolada <pinoydol.colada@gmail.com>
2023-06-09 23:14:32 -0400

    changed project button to download my resume

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -8,11 +8,11 @@ const HeroSection = () => {
     <section id="home">
       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
         <div className="md:mt-2 md:w-1/2">
-        <Image className="rounded-full shadow-2x1" src="/headshotpic.jpg" alt="profile photo" width={300} height={300}/>
+          <Image className="rounded-full shadow-2x1" src="/headshotpic.jpg" alt="profile photo" width={300} height={300} />
         </div>
 
         <div className="md:mt-2 md:w-3/5">
-        <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Ryan!</h1>
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Ryan!</h1>
           <p className="text-lg mt-4 mb-6 md:text-2xl">
             I&#39;m a{" "}
             <span className="font-semibold text-teal-600">
@@ -21,17 +21,9 @@ const HeroSection = () => {
             based in Beltsville, MD. Working towards creating software that
             makes life easier and more meaningful.
           </p>
-          <Link
-            to="projects"
-            className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
-            activeClass="active"
-            spy={true}
-            smooth={true}
-            offset={-100}
-            duration={500}
-          >
-            Projects
-          </Link>
+          <a href="/cv.pdf" download="cv" className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700 margin-left: 4px;">
+            Download My Resume
+          </a>
         </div>
       </div>
       <div className="flex flex-row items-center text-center justify-center ">
