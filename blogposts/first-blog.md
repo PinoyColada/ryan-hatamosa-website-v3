c5021850693e87d1ddcfac7fe8e5b8c7bfcb8ac9
pinoycolada <pinoydol.colada@gmail.com>
2023-07-14 23:32:17 -0400

    added blogpost on navbar, created blog post directory

---
+++ blogposts/first-blog.md
@@ -0,0 +1,6 @@
+---
+title: 'First Blog Post!'
+date '2023-07-14'
+---
+
+This is a test post
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,6 +28,10 @@ const NAV_ITEMS: Array<NavItem> = [
     label: "Projects",
     page: "projects",
   },
+  {
+    label: "Blog",
+    page: "blog"
+  }
 ];
 
 const Navbar = () => {
