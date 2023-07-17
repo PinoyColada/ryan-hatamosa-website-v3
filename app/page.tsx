69647af24dca80902d82d858b7c9f943c1aa3234
pinoycolada <pinoydol.colada@gmail.com>
2023-07-16 22:57:58 -0400

    modified md file to mdx, changed navbar component location

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import HeroSection from "@/components/HeroSection"
 import AboutSection from "@/components/AboutSection"
 import ProjectsSection from "@/components/ProjectsSection"
 import Posts from "@/components/Posts"
+import Navbar from "@/components/Navbar"
 
 export default function Home() {
   return (
+    <>
+    <Navbar />
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
       <HeroSection/>
       <AboutSection/>
       <Posts/>
       <ProjectsSection/>
     </main>
+    </>
   )
 }
