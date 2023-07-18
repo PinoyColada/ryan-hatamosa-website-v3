93896d55efafb5b8ee04c8c7de079b4703a7f3e9
pinoycolada <pinoydol.colada@gmail.com>
2023-07-18 12:48:12 -0400

    added logic to check if no posts exist in post component

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import { getSortedPostsData } from '@/lib/posts'
+import { getPostsMeta } from '@/lib/posts'
 import ListItem from "./ListItem"
 
-const Posts = () => {
-    const posts = getSortedPostsData()
+const Posts = async () => {
+    const posts = await getPostsMeta()
+
+    // checks if they are no posts
+    if (!posts) {
+        return <p className="mt-10 text-center"> Sorry, no posts available.</p>
+    }
 
     return (
         <section id="blog" className="mt-6 mx-auto max-w-2xl">
@@ -11,7 +16,7 @@ const Posts = () => {
                 Blog
                 <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
             </h1>
-            <ul className="w-full">
+            <ul className="w-full list-none p-0">
                 {posts.map(post => (
                     <ListItem key={post.id} post={post} />
                 ))}
