e4be08c24238121c563081fd23c9c01b0dc6f524
Ryan Hatamosa <pinoydol.colada@gmail.com>
2023-08-26 16:28:29 -0400

    changed the blog sectionin a different order, updated posts file

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,28 +1,39 @@
-import React from 'react'
-import { getPostsMeta } from '@/lib/posts'
-import ListItem from "./ListItem"
+import React from "react";
+import { getPostsMeta } from "@/lib/posts";
+import ListItem from "./ListItem";
 
 const Posts = async () => {
-    const posts = await getPostsMeta()
+  const posts = await getPostsMeta();
 
-    // checks if they are no posts
-    if (!posts) {
-        return <p className="mt-10 text-center"> Sorry, no posts available.</p>
-    }
-
+  // checks if they are no posts
+  if (!posts) {
     return (
-        <section id="blog" className="mt-6 mx-auto max-w-2xl">
-            <h1 className="text-center font-bold text-4xl">
-                Blog
-                <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
-            </h1>
-            <ul className="w-full list-none p-0">
-                {posts.map(post => (
-                    <ListItem key={post.id} post={post} />
-                ))}
-            </ul>
-        </section>
-    )
-}
+      <section id="blog" className="mt-6 mx-auto max-w-2xl">
+        <h1 className="text-center font-bold text-4xl">
+          Blog
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+        <p className="mt-10 text-center">
+          {" "}
+          Sorry, there are no posts available at the moment.
+        </p>
+      </section>
+    );
+  }
 
-export default Posts
+  return (
+    <section id="blog" className="mt-6 mx-auto max-w-2xl">
+      <h1 className="text-center font-bold text-4xl">
+        Blog
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+      </h1>
+      <ul className="w-full list-none p-0">
+        {posts.map((post) => (
+          <ListItem key={post.id} post={post} />
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export default Posts;
